b5ec389b88788b9d8483c5f4a7ce9203e864cbff
Vadim <vadimmalakhovski@hotmail.com>
2018-04-13 09:11:11 +0000

    deploy heroku 3

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,10 @@ module Workspace
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    
+    config.web_console.whitelisted_ips = '109.78.185.139'
+    config.web_console.whiny_requests = false
+    config.web_console.development_only = false
+    config.assets.initialize_on_precompile = false
   end
 end
